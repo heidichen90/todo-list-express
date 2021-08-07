62e5e341876bf9dc2252e29f9aabcaf6bb6b3e48
heidichen90 <toddypet@gmail.com>
2021-08-07 12:52:16 +0100

    feature/add express-session

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
-const express = require('express')
-const exphbs = require('express-handlebars')
-const bodyParser = require('body-parser')
-const methodOverride = require('method-override')
+const express = require("express");
+const exphbs = require("express-handlebars");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
 
-const routes = require('./routes')
-require('./config/mongoose')
+const routes = require("./routes");
+require("./config/mongoose");
 
-const app = express()
-const PORT = process.env.PORT || 3000
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
-app.set('view engine', 'hbs')
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", "hbs");
 
-app.use(routes)
+app.use(
+  session({
+    secret: "ThisIsMySecret",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+app.use(routes);
 
 app.listen(PORT, () => {
-  console.log(`App is running on http://localhost:${PORT}`)
-})
+  console.log(`App is running on http://localhost:${PORT}`);
+});
